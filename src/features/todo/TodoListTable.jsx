2cc74950f28ab0aee27de08100f70f630d30fcb9
Anh Tuan Truong <tanhtruong97@gmail.com>
2023-10-11 08:59:37 +0200

    Generic Todo Tables
    - Tables moved to a generic table jsx-file

---
+++ src/features/todo/TodoListTable.jsx
@@ -0,0 +1,41 @@
+import { useDispatch } from "react-redux";
+import { Container, Col, Table } from "react-bootstrap";
+import { toggleCompleteness } from "./todoSlice";
+
+export default function TodoListTable({ todos, numOfTodos }) {
+	const dispatch = useDispatch();
+
+	return (
+		<>
+			<Container className='mt-4'>
+				<h2>Todos: {numOfTodos}</h2>
+				<Col>
+					<Table striped hover>
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Title</th>
+							</tr>
+						</thead>
+						<tbody>
+							{todos.map((todo) => {
+								return (
+									<tr key={todo.id}>
+										<td>{todo.id}</td>
+										<td
+											className={todo.completed === true ? "completed" : ""}
+											onClick={() =>
+												dispatch(toggleCompleteness({ id: todo.id }))
+											}>
+											{todo.title}
+										</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</Table>
+				</Col>
+			</Container>
+		</>
+	);
+}
--- src/features/todo/TodoListView.jsx
+++ src/features/todo/TodoListView.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchTodos, toggleCompleteness } from "./todoSlice";
-import { Container, Col, Table } from "react-bootstrap";
+import { fetchTodos } from "./todoSlice";
 import "./TodoListView.css";
 import { useLocation, useParams } from "react-router-dom";
+import TodoListTable from "./TodoListTable";
 
 export default function TodoListView() {
 	const todos = useSelector((state) => state.todo.todos);
@@ -19,72 +19,14 @@ export default function TodoListView() {
 
 	return (
 		<>
-			<Container className='mt-4'>
-				<h2>Todos: {uncompletedTodos}</h2>
-				<Col>
-					<Table striped hover>
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Title</th>
-							</tr>
-						</thead>
-						<tbody>
-							{todos.map((todo) => {
-								if (!todo.completed) {
-									return (
-										<tr key={todo.id}>
-											<td>{todo.id}</td>
-											<td
-												className={
-													todo.completed === true ? "completed" : ""
-												}
-												onClick={() =>
-													dispatch(toggleCompleteness({ id: todo.id }))
-												}>
-												{todo.title}
-											</td>
-										</tr>
-									);
-								}
-							})}
-						</tbody>
-					</Table>
-				</Col>
-			</Container>
-			<Container className='mt-4'>
-				<h2>Completed todos: {completedTodos}</h2>
-				<Col>
-					<Table striped hover>
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Title</th>
-							</tr>
-						</thead>
-						<tbody>
-							{todos.map((todo) => {
-								if (todo.completed) {
-									return (
-										<tr key={todo.id}>
-											<td>{todo.id}</td>
-											<td
-												className={
-													todo.completed === true ? "completed" : ""
-												}
-												onClick={() =>
-													dispatch(toggleCompleteness({ id: todo.id }))
-												}>
-												{todo.title}
-											</td>
-										</tr>
-									);
-								}
-							})}
-						</tbody>
-					</Table>
-				</Col>
-			</Container>
+			<TodoListTable
+				todos={todos.filter((todo) => !todo.completed)}
+				numOfTodos={uncompletedTodos}
+			/>
+			<TodoListTable
+				todos={todos.filter((todo) => todo.completed)}
+				numOfTodos={completedTodos}
+			/>
 		</>
 	);
 }
